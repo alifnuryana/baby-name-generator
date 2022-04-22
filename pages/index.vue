5cdc63b5feae2c1a6c60a7b0e6e79e7294708311
Alif Nuryana <alifnuryana0@gmail.com>
2022-04-22 22:46:36 +0700

    showing resultName and styling index.vue file

--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,7 @@ const computeResultNames = () => {
       else return name.length === options.length;
     });
   resultName.value = filteredNames.map((name) => name.name);
+  window.scrollTo(0, document.body.scrollHeight);
 };
 </script>
 
@@ -111,7 +112,12 @@ const computeResultNames = () => {
       </div>
       <button class="primary" @click="computeResultNames">Find Names</button>
     </div>
-    {{ resultName }}
+    <div class="cards-container">
+      <div class="card" v-for="name in resultName" :key="name">
+        <h4>{{ name }}</h4>
+        <p>x</p>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -128,7 +134,6 @@ h1 {
   width: 95%;
   margin: 0 auto;
   margin-top: 4rem;
-  margin-bottom: 4rem;
   position: relative;
 }
 
@@ -170,5 +175,35 @@ h1 {
   font-size: 1rem;
   margin-top: 1rem;
   cursor: pointer;
+}
+
+.cards-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+
+.card p {
+  position: absolute;
+  width: 1.2rem;
+  text-align: center;
+  border-radius: 50%;
+  top: -32%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgb(27, 60, 138);
+  background-color: rgb(249, 87, 89);
 }
 </style>
